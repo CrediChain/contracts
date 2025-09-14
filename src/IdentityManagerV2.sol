014fea6b763275029cfb69386cdd38421dd51737
malgus01 <mlghectiik@gmail.com>
2025-09-13 17:29:57 -0700

    feat: Add Necessary Import Dependencies

--- src/IdentityManagerV2.sol
+++ src/IdentityManagerV2.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+import {ByteHasher} from "./helpers/ByteHasher.sol";
+import {IWorldID} from "./interfaces/IWorldID.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+
 contract IdentityManagerV2 {}
