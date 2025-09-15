7c411cd75d07e7b4a5ede6b4521a0850615d4260
flasharbai-intern <flasharbai@gmail.com>
2025-09-14 19:14:09 -0700

    feat: Add List of all verified users for enumeration

--- src/IdentityManagerV2.sol
+++ src/IdentityManagerV2.sol
@@ -95,5 +95,6 @@ contract IdentityManagerV2 is AccessControl, ReentrancyGuard, Pausable {
         /// @dev Default verification expiration time (1 year in seconds)
     uint256 public constant DEFAULT_EXPIRATION = 365 days;
 
-
+    /// @dev List of all verified users for enumeration
+    address[] public verifiedUsers;
 }
