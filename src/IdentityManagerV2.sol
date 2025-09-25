4ab8a6cc2448fe4f094796ce8dbd559956ca0698
tyranis0x01 <tyranis0x01@gmail.com>
2025-09-25 08:30:49 -0700

    feat: Add _removeFromUsersByType Internal Function

--- src/IdentityManagerV2.sol
+++ src/IdentityManagerV2.sol
@@ -559,4 +559,18 @@ contract IdentityManagerV2 is AccessControl, ReentrancyGuard, Pausable {
         userPositions[userType][user] = usersByType[userType].length;
         usersByType[userType].push(user);
     }
+
+    function _removeFromUsersByType(address user, UserType userType) internal {
+        uint256 position = userPositions[userType][user];
+        uint256 lastPosition = usersByType[userType].length - 1;
+
+        if (position != lastPosition) {
+            address lastUser = usersByType[userType][lastPosition];
+            usersByType[userType][position] = lastUser;
+            userPositions[userType][lastUser] = position;
+        }
+
+        usersByType[userType].pop();
+        delete userPositions[userType][user];
+    }
 }
