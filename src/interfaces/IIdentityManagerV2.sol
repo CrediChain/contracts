ef05dc23e151ccdf8f1ef265ec1780047461e1a7
tyranis0x01 <tyranis0x01@gmail.com>
2025-09-28 11:27:57 -0700

    feat: Add Contract Title Natspec Comments

--- src/interfaces/IIdentityManagerV2.sol
+++ src/interfaces/IIdentityManagerV2.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+/**
+ * @title IIdentityManagerV2
+ * @notice Interface for the enhanced IdentityManagerV2 contract
+ * @dev This interface defines all external functions for identity verification management
+ */
 interface IIdentityManagerV2 {}
