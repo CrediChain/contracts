af1453cf8430ca58cb9314f4378086c37dd0a123
Shawn Rizo <shawnrizo@hotmail.com>
2025-09-29 14:04:20 -0400

    feat: Add event VerificationRenewed

--- src/interfaces/IIdentityManagerV2.sol
+++ src/interfaces/IIdentityManagerV2.sol
@@ -50,5 +50,6 @@ interface IIdentityManagerV2 {
 
     event UserVerificationRevoked(address indexed user, address indexed revoker, string reason);
     event UserTypeUpdated(address indexed user, UserType oldType, UserType newType);
+    event VerificationRenewed(address indexed user, uint256 newExpirationTimestamp);
     event BatchVerificationCompleted(address indexed admin, uint256 count, UserType userType);
 }
