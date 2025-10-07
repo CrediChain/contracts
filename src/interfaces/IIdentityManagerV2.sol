0b934b3c3f923ae379c4230e06afe317d47f57a9
tyranis0x01 <tyranis0x01@gmail.com>
2025-10-07 08:08:05 -0700

    feat: forge fmt

--- src/interfaces/IIdentityManagerV2.sol
+++ src/interfaces/IIdentityManagerV2.sol
@@ -85,6 +85,5 @@ interface IIdentityManagerV2 {
     function isVerificationExpired(address user) external view returns (bool);
     function getUserVerification(address user) external view returns (UserVerification memory);
     function getVerificationLevel(address user) external view returns (VerificationLevel);
-        function getUserType(address user) external view returns (UserType);
-
+    function getUserType(address user) external view returns (UserType);
 }
